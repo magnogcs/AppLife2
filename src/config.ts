96302b309601614c5aabbb3a8a9b7cd17f2c5247
Magno Guimarães <magnogcs@gmail.com>
2018-02-15 17:58:21 -0300

    back done

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 //config constants
-export const WORDPRESS_URL = 'http://localhost/wordpress/';
+export const WORDPRESS_URL = 'http://192.95.3.233/wordpress/';
 export const WORDPRESS_REST_API_URL = WORDPRESS_URL + 'wp-json/wp/v2/';
